4df4f56046a24c4f7e9b27dc92bbb50666a43f09
rosesenang <99687665+rosesenang@users.noreply.github.com>
2024-10-28 16:23:03 +0000

    backend correction.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "iot-proj-terraform-state-bucket"    # S3 bucket name
     key            = "health-wearables-iot/terraform.tfstate"
-    region         = "us-east-1"                      # Region
+    region         = "eu-west-2"                      # Region
     dynamodb_table = "terraform-state-lock"           # DynamoDB table for state locking
     encrypt        = true                             # Encrypts the state file at rest
   }
